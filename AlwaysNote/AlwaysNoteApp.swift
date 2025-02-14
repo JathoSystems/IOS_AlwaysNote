2cd7646e8585f5e3ca268bb8df1e786b7129675f
Thomas Versteeg <t.versteeg2@student.avans.nl>
2025-02-14 10:12:51 +0100

    Initial Commit

---
+++ AlwaysNote/AlwaysNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysNoteApp.swift
+//  AlwaysNote
+//
+//  Created by Thomas Versteeg on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AlwaysNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlwaysNoteTests/AlwaysNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlwaysNoteTests.swift
+//  AlwaysNoteTests
+//
+//  Created by Thomas Versteeg on 14/02/2025.
+//
+
+import Testing
+@testable import AlwaysNote
+
+struct AlwaysNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
